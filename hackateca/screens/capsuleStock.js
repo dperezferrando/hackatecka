4cd55ea6a6ab6d69e9684ae08c666da9aa18fc4f
dperezferrando <diego@parsimotion.com>
2020-03-18 22:09:58 -0300

    State

--- hackateca/screens/capsuleStock.js
+++ hackateca/screens/capsuleStock.js
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 class CapsuleStock extends React.PureComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = { cantidad: this.props.item.cantidad}
+  }
   render() {
     const { item } = this.props;
-    return <Text>{item.nombre} - {item.cantidad}</Text>
+    return <View>
+      <Text>{item.nombre} - {this.state.cantidad}</Text>
+      <Button onPress={() => this.increment()} title="+"/>
+      <Button onPress={() => this.decrement()} title="-"/>
+    </View>
+  }
+
+  increment() {
+    this.setState({ ...this.state, cantidad: this.state.cantidad + 1 })
+  }
+
+  decrement() {
+    this.setState({ ...this.state, cantidad: this.state.cantidad - 1 })
   }
 }
 
