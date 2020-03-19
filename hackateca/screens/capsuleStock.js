f22b81d53f3ffa68d6a5ac13e0b30efb8b5b95bb
dperezferrando <diego@parsimotion.com>
2020-03-19 00:32:55 -0300

    Estilos

--- hackateca/screens/capsuleStock.js
+++ hackateca/screens/capsuleStock.js
@@ -1,5 +1,39 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  stock: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "lightgrey",
+    margin: 3,
+    justifyContent: "space-between"
+
+  },
+  nombre: {
+    margin: 5,
+    fontSize: 20
+  },
+  cantidad: {
+    margin: 5,
+    fontSize: 20,
+    color: "#2e78b7",
+    fontWeight: "bold"
+  },
+  container: {
+    flexDirection: "row"
+  },
+  button: {
+    margin: 5,
+    backgroundColor: "#2e78b7",
+    width: 20,
+    alignItems: "center",
+    color: "white",
+    borderRadius: 3,
+    textAlignVertical: "center"
+  }
+
+});
 
 class CapsuleStock extends React.PureComponent {
 
@@ -9,10 +43,19 @@ class CapsuleStock extends React.PureComponent {
   }
   render() {
     const { item } = this.props;
-    return <View>
-      <Text>{item.nombre} - {this.state.cantidad}</Text>
-      <Button onPress={() => this.increment(1)} title="+"/>
-      <Button onPress={() => this.increment(-1)} title="-"/>
+    return <View style={styles.stock}>
+      <View style={styles.container}>
+        <Text style={styles.nombre}>{item.nombre}</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.cantidad}>{this.state.cantidad}</Text>
+        <TouchableOpacity style={styles.button} onPress={() => this.increment(1)}> 
+          <Text> + </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.increment(-1)}>
+          <Text> - </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   }
 
--- hackateca/screens/stockList.js
+++ hackateca/screens/stockList.js
@@ -8,36 +8,13 @@ import CapsuleStock from "./capsuleStock";
 
 export default () => {
   return (
-    <FlatList data={capsulitas} renderItem={({ item }) => <CapsuleStock item={item} />}/>
+    <FlatList data={capsulitas} style={styles.lista} renderItem={({ item }) => <CapsuleStock item={item} />}/>
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
+  lista: {
+    marginTop: 10
+  }
 });
