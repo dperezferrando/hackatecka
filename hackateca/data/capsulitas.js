ba794857a879b77a3e730f1fc842122c23dd5642
dperezferrando <diego@parsimotion.com>
2020-03-18 21:53:07 -0300

    Data

---
+++ hackateca/data/capsulitas.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    "nombre": "Ristretto",
+    "cantidad": 10  
+  },
+  {
+    "nombre": "Vivalto",
+    "cantidad": 3  
+  },
+  {
+    "nombre": "Arpeggio",
+    "cantidad": 5  
+  },
+  {
+    "nombre": "Roma",
+    "cantidad": 7  
+  },
+  {
+    "nombre": "Corona Coffee-19",
+    "cantidad": 5  
+  },
+  {
+    "nombre": "Ispirazione Venezia",
+    "cantidad": 556
+  }
+]
